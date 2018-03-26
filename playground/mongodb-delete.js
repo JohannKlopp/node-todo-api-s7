d0f7f47ff802ac513a847c87e1e4e6c967b4aaf2
Johann Klopp <clouduser@i-791.localdomain>
2018-03-26 09:10:25 +0100

    add JSON.stringify to second active (not commented out) delete scripts

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,4 +1,4 @@
-// const MongoClient = require("mongodb").MongoClient;
+// const MongoClient = require("mongodb").MongoClient; *is the same as below, except for the ObjectID*
 const {MongoClient, ObjectID} = require("mongodb");
 
 MongoClient.connect("mongodb://172.18.1.26:27017/TodoApp", (err, client) => {
@@ -25,15 +25,16 @@ MongoClient.connect("mongodb://172.18.1.26:27017/TodoApp", (err, client) => {
   // });
 
 //1. look for duplicates, use deletemany to remove all those docs
-  db.collection("Users").deleteMany({name: "Johann Klopp"}).then((result) => {
-    console.log(result.result);
+  db.collection("Users").deleteMany({name: "Johann"}).then((results) => {
+    console.log(`You just deleted ${results.result.n} documents.`); /*this just returns {n: x, ok:1} */
   });
 
 //2. use findone to delete one other doc by ID
   db.collection("Users").findOneAndDelete({
-    _id: new ObjectID("5ab26824fbe41c268e24a8e6")
-  }).then((result) => {
-    console.log(result);
+    _id: new ObjectID("5ab8a932518ab7ae3cc8efd4")
+  }).then((results) => {
+    //console.log(JSON.stringify(results, undefined, 2));
+    console.log(results);
   });
 
   // client.close();
